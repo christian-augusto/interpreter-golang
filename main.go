318f02faafbde982d8d13e1916a596816a2c353e
Christian Augusto Martins dos Santos Barros <christianbetta@gmail.com>
2022-05-08 12:35:55 -0300

    feat: adding read code from file

--- main.go
+++ main.go
@@ -1,5 +1,44 @@
 package main
 
+import (
+	"fmt"
+	"interpreter-golang/dependencies"
+	"interpreter-golang/interpreter"
+	"interpreter-golang/logger"
+	"os"
+)
+
 func main() {
-	// var logger dependencies.Logger = logger.NewLogger()
+	var logger dependencies.Logger = logger.NewLogger()
+	var err error
+
+	if len(os.Args) < 2 {
+		err = fmt.Errorf("Missing file path to read the code")
+		logger.Error(err)
+		os.Exit(1)
+	}
+
+	filePath := os.Args[1]
+	var codeBytes []byte
+	var code string
+
+	codeBytes, err = os.ReadFile(filePath)
+
+	if err != nil {
+		logger.Error(err)
+		os.Exit(1)
+	}
+
+	code = string(codeBytes)
+
+	fmt.Println(code)
+
+	inter := interpreter.NewInterpreter(code)
+
+	err = inter.Start()
+
+	if err != nil {
+		logger.Error(err)
+		os.Exit(1)
+	}
 }
